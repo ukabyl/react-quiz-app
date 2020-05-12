1bb63c29f3939d314abb0b4d0c5535974111006f
ukabyl <umkabylbekov@gmail.com>
2020-05-12 21:46:52 +0500

    Updates

---
+++ precache-manifest.78cf28d66575d7105b7f95e087072f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86df2a54804ff645f9ed0d75ab5c0f94",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "baad76ef4c68304b287a",
+    "url": "/react-quiz-app/static/css/main.8da80398.chunk.css"
+  },
+  {
+    "revision": "48f5a31b8e5916818442",
+    "url": "/react-quiz-app/static/js/2.14b610f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz-app/static/js/2.14b610f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baad76ef4c68304b287a",
+    "url": "/react-quiz-app/static/js/main.d1516662.chunk.js"
+  },
+  {
+    "revision": "d630b9a8421f5e0175ae",
+    "url": "/react-quiz-app/static/js/runtime-main.c9e4a93f.js"
+  }
+]);
